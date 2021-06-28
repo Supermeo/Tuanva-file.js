e0c0ae1bcd33e20e400ff5678fed1245715d97f4
Vi Anh Tuân <84442807+Supermeo@users.noreply.github.com>
2021-06-28 08:44:08 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /* ví dụ về true false
 
 let x;
-x = 1;   // x la number
+x = 1;   // x la number 
 x = '1';   // x la string
 x = [1];   // x la array
 
@@ -10,22 +10,43 @@ x = [1];   // x la array
 1 = [1];   // false
 */
 
-/*các loại vòng lặp
 
--------------------------------------vòng lặp for
+/* các loại vòng lặp
 
+-------------------------------------vòng lặp for ----------------------------
+**** Vòng lặp for dùng để lặp lại việc thực thi một đoạn mã nào đó một số lần.
 vòng lặp for ( lưu ý )
 break và continue
 break : code chạy đến break > dừng ( kết thúc vòng lặp)
 continue : bỏ qua 1 bước lặp ( bỏ qua 1 bước lặp không chạy bước lặp đã đánh dấu continue)
 
 ví dụ :
-for (var i =0; i < 10) {
-    console.log(i)
-    i++
-}
 
--------------------------------------vòng lặp while
+ for (var i = 0; i < 10; i++) {
+     console.log(i);
+ }
+
+ ----------------Vòng lặp For ( break )-----------------------------
+
+ for (var i = 0; i < 10; i++) {
+     console.log(i);
+
+     if(i>=5){
+         break;
+     }
+  }
+
+ ------------------Vòng lặp For ( continue )--------------------------
+
+
+for (var i = 0; i < 10; i++) {
+    if(i % 2 ==0){
+    continue
+    }
+    console.log(i)
+    }
+  
+-------------------------------------vòng lặp while ----------------------------
 
 đưa ra 1 điều kiện nếu điều kiện k thỏa mãn ( hoặc sai ) > kết thúc vòng lặp, còn nếu vòng lặp đúng > tiếp tục vòng lặp
 (vòng lặp while dùng để : lặp lại việc thực hiện một đoạn mã nếu điều kiện mà mình đưa ra vẫn còn đúng)
@@ -38,7 +59,7 @@ while(i < 10) {
     i++;
 }
 
-vòng ;ặp do while (while và do while gần như giống nhau nhưng với do while thì điều kiện lặp như thế nào thì
+vòng lặp do while (while và do while gần như giống nhau nhưng với do while thì điều kiện lặp như thế nào thì
 code vẫn chạy ít nhất 1 lần, còn điều kiện thỏa mãn thì tương tự như while => tạo thêm vòng lặp)
 
 ví dụ 
@@ -50,53 +71,54 @@ do {
 }
 while ( i > 10 && i < 12)
 
-------------------------------------- chuỗi > các hàm xử lí chuỗi-------------------------------------------------------------------
+------------------------------------- chuỗi > các hàm xử lí chuỗi-----------------------------
 
-indexOf;
+*****  indexOf;
 
 trả giá trị về vị trí đầu tiên của chuỗi cần tìm trong chuỗi gốc, nếu trong chuỗi không có giá trị cần tìm thì nó sẽ trả về -1
 
-function name {
-  var name = ['Tuan', 'Duy', 'Kien', 'Hai', 'Duong'];
-  var a = name.indexOf('Tuan');
+vd 
 
-lastIndexOf(tên chuỗi): trả giá trị về vị trí cuối cùng của chuỗi cần tìm trong chuỗi gốc
+var name = ['Tuan', 'Duy', 'Kien', 'Hai', 'Duong'];
+var a = name.indexOf('Kien');
+console.log(a)
 
-function name {
-  var name = ['Tuan', 'Duy', 'Kien', 'Hai', 'Duong'];
-  var a = name.lastIndexOf('Duong');
+***   lastIndexOf : trả giá trị về vị trí cuối cùng của chuỗi cần tìm trong chuỗi gốc
 
-search  tìm kiếm 1 chuỗi con trong chuỗi bố và trả về vị trí ( số nó đang nằm) ( nếu không thấy sẽ trả về -1)
+vd
+var name = ['Tuan', 'Duy', 'Kien', 'Hai', 'Duong'];
+var a = name.lastIndexOf('');
+console.log(a)
 
-function name {
-  var name = ['Tuan', 'Duy', 'Kien', 'Hai', 'Duong'];
-  var a = name.search('Duong');
-  => kq = 5
+*** search tìm kiếm 1 chuỗi con trong chuỗi bố và trả về vị trí ( số nó đang nằm) ( nếu không thấy sẽ trả về -1)
 
+vd
+
+var a = 'toi, la, vi';
+console.log(a.search('vi'))
 
 ----------------------------------------- chuỗi > các hàm cắt chuỗi-------------------------------------------------------------------
 
-+ slice : cắt chuỗi và trả về giá trị là một chuỗi mới
-begin là vị trí bắt đầu cắt chuỗi
-end là vị trí kết thúc cắt chuỗi (nếu không điền thì là cắt đến hết chuỗi).
+*** slice : cắt chuỗi và trả về giá trị là một chuỗi mới
+* begin là vị trí bắt đầu cắt chuỗi
+* end là vị trí kết thúc cắt chuỗi (nếu không điền thì là cắt đến hết chuỗi).
 vd
 
 var a = 'vi anh tuan dep trai';
-a.slice(9, 15);
-
+console.log(a.slice(12, 20))
 
 + substring : Cũng tương tự như hàm slice, nhưng tham số truyền vào phải lớn hơn 0 ( không được truyền vào số âm)
 vd 
 
 var a = 'vi anh tuan dep trai';
-a.substring(0, 8);
+console.log(a.substring(0, 11))
 
-+replace : Phương thức này cho phép chúng ta tìm kiếm và thay thế chuỗi
++ replace : Phương thức này cho phép chúng ta tìm kiếm và thay thế chuỗi
 
 vd
 
 var a = 'vi anh tuan dep trai';
-a.replace('dep trai', rickid);
+console.log(a.replace('dep trai', 'rickid'));	
 
 +length : dùng để lấy độ dài của chuỗi trong javascrip
 
@@ -104,17 +126,15 @@ vd
 
 var a = 'vi anh tuan dep trai';
 var ham = a.length;
-console.log(count);
+console.log(a.length);
 
 +charAt : dùng để trả về ký tự tại một vị trí được xác định trong chuỗi
 vd
 
 var a = 'vi anh tuan dep trai';
-a.charAt(0,1);
+console.log(a.charAt('0, 1'));
 
 > vi
-
-
 
 ----------------------------------------- Hàm xử lí mảng-------------------------------------------------------------------
 
@@ -123,7 +143,8 @@ a.charAt(0,1);
 vd 
 
 var a = ['vi anh tuan'];
-a.push(dep trai);
+  a.push('dep trai');
+  console.log(a)
 
 => vi anh tuan dep trai
 
@@ -131,26 +152,30 @@ a.push(dep trai);
 
 vd 
 
-var a = ['vi' 'anh' 'tuan' 'dep' 'trai'];
-a.pop(4,5);
+var a = ['vi', 'anh', 'tuan', 'dep', 'trai'];
+a.pop();
+console.log(a)
 
-=> vi anh tuan
+=> vi anh tuan dep
 
 Hàm array.shift : hàm cắt phần tử đầu tiên của mảng, sau đó dồn các phần tử phía sau xuống một bậc
 
 vd 
 
-var a = ['vi' 'anh' 'tuan' 'dep' 'trai'];
-a.shift(1, 2, 3);
+var a = ['vi', 'anh', 'tuan', 'dep', 'trai'];
+a.shift();
+console.log(a);
 
-=> dep trai
+
+=> anh tuan dep trai
 
 Hàm array.unshift : thêm một phần tử vào vị trí đầu tiên của mảng, đồng thời đẩy các phẩn từ phía sau lên một bậc
 
 vd
 
-var a = ['vi' 'anh' 'tuan' 'dep' 'trai'];
-a.unshift(hello);
+var a = ['vi', 'anh', 'tuan', 'dep', 'trai'];
+a.unshift('hello');
+console.log(a)
 
 => hello vi anh tuan dep trai;
 
@@ -158,28 +183,29 @@ a.unshift(hello);
 
 vd
 
-var a = ['vi' 'anh' 'tuan' 'dep' 'trai'];
-a.slice(1, 3);
-
-=> trai 
+var a = ['vi', 'anh', 'tuan', 'dep', 'trai'];
+console.log(a.slice(1, 2));
 
 + Hàm array.splice : hàm splice gần giống nhàm slice ( khác = cắt hết tất cả phần tử từ phần đánh dấu, khi cắt sẽ ảnh hưởng đến mảng ban đầu)
 
 vd
 
-var a = ['vi' 'anh' 'tuan' 'dep' 'trai'];
-a.splice(3, 5);
+var a = ['vi', 'anh', 'tuan', 'dep', 'trai'];
+a.splice(1, 2);
+console.log(a);
 
-=> vi anh tuan
+=> vi dep trai
 
-2 hàm slice và splice đều giống nhau ở điểm là dùng đề cắt chuỗi
-
+2 hàm slice và splice đều giống nhau ở điểm là dùng đề cắt chuỗi =======================================================
 
 + Hàm array.reverse : hàm đảo ngược các phẩn tử (đầu > cuối, cuối > đầu)
 
 vd
-var a = ['vi' 'anh' 'tuan' 'dep' 'trai'];
-a.reverse();
+
+var a = ['vi', 'anh', 'tuan', 'dep', 'trai'];
+a.reverse(3);
+console.log(a);
+
 
 => trai dep tuan anh vi
 
@@ -187,12 +213,14 @@ a.reverse();
 + Hàm array.concat : hàm dùng để nối hai mảng với nhau và trả về một mảng gồm tổng số phần tử của hai mảng đó
 
 vd
-var a = ['vi' 'anh' 'tuan'];
-var b = ['dep' 'trai'];
-var b = a.concat(b);
+
+var a = ['vi', 'anh', 'tuan'];
+var b = ['dep', 'trai'];
+var c = a.concat(b);
+console.log(c)
 
 => vi anh tuan dep trai
 
 
-
 */
+
